28978b4b57e6754890e41a7441f19c77a02e8cb5
orgil-munkh <batorgil@motforex.com>
2026-02-13 17:48:41 +0800

    fix nextjs config

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,12 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    SUPABASE_SERVICE_ROLE_KEY: process.env.SUPABASE_SERVICE_ROLE_KEY,
+    DATABASE_URL: process.env.DATABASE_URL,
+  }
 };
 
 export default nextConfig;
